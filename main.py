b509362d2d72c87730940634b5f424b382b4bfcf
swethatheresa <swethatheresa123@gmail.com>
2023-05-02 17:27:34 +0530

    OAuth security using FastAPI

--- main.py
+++ main.py
@@ -1,45 +1,13 @@
-from fastapi import FastAPI,Query,Path
-from pydantic import BaseModel
 from typing import Annotated
 
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float 
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-app=FastAPI()
-@app.get("/")
-async def root():
-    return { 'message':'hello world'}
-
-@app.get('/items/{item_id}')
-async def item(item_id : int):
-    return {'item_id':item_id}
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
-
-@app.get("/test/")
-async def read_items(
-    q: Annotated[
-        str | None,
-        Query(
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
-            alias="item-query",
-        ),
-    ] = None
-):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}

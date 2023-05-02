a694d43cb2ce701d4420a6ae162cb522b78a5316
swethatheresa <swethatheresa123@gmail.com>
2023-05-02 13:29:14 +0530

    path,query,body parameters tested

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float 
+
 
 app=FastAPI()
 @app.get("/")
 async def root():
     return { 'message':'hello world'}
+
+@app.get('/items/{item_id}')
+async def item(item_id : int):
+    return {'item_id':item_id}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
